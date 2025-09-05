73bac816a0e3a4709ce2e48c962b7eeb30d2223c
Caspianen <mikesanta51@gmail.com>
2025-09-05 20:49:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// kontrol mouse
+canvas.addEventListener("mousedown", e => {
+  attract = true;
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+canvas.addEventListener("mouseup", () => {
+  attract = false;
+});
+canvas.addEventListener("mousemove", e => {
+  if (attract) {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+  }
+});
 
+// kontrol touch (HP)
+canvas.addEventListener("touchstart", e => {
+  attract = true;
+  mouse.x = e.touches[0].clientX;
+  mouse.y = e.touches[0].clientY;
+});
+canvas.addEventListener("touchend", () => {
+  attract = false;
+});
+canvas.addEventListener("touchmove", e => {
+  if (attract) {
+    mouse.x = e.touches[0].clientX;
+    mouse.y = e.touches[0].clientY;
+  }
+});
